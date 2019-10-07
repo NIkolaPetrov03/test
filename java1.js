20f3103fa9b2e4c1f4afacac2124341025eba069
NikolaPetrov123 <54896512+NikolaPetrov123@users.noreply.github.com>
2019-10-07 08:46:56 -0400

    64tym t

---
+++ java1.js
@@ -0,0 +1,10 @@
+function Angles() {
+	var user_firstAngle= prompt('Please Enter Your First Angle');
+	var user_secAngle= prompt('Please Enter Your Second Angle');
+	if(180-user_firstAngle-user_secAngle>=0){
+		alert(180 - user_firstAngle - user_secAngle);
+	}
+	else if (180 - user_firstAngle - user_secAngle <=0)
+		alert('Not Valid, Please Try Again')
+
+	}
